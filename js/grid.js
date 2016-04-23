ff36bc979a5d04e39920f2cc49185b88f8694cb7
Anthony Diep <diepa1@uci.edu>
2016-04-22 20:44:23 -0700

    implemented prompt for new grid size on clear

--- js/grid.js
+++ js/grid.js
@@ -1,25 +1,41 @@
 var html = "<div class='grid-box'></div>";
+var gridSize = 16;
 
 $(document).ready(function() {
-  for (var i = 0; i < 16; i++) {
-    for (var j = 0; j < 16; j++) {
-      $(".grid-container").append(html);
-    }
-
-  }
-
-  $(".grid-box").hover(function() {
-    $(this).addClass("fill");
-  });
+  spawnGrid(gridSize);
 
   $("#clear").click(function() {
     $(".grid-container").children(".grid-box").each(function() {
       $(this).removeClass("fill");
     });
+
+    gridSize = parseInt(prompt("Enter the desired size for the new grid between 0 and 64.", gridSize.toString()));
+    spawnGrid(gridSize);
   });
 
   $("#new").click(function() {
 
   });
+
+  function spawnGrid(size) {
+    if (size < 0) {
+      size = 16;
+    }
+
+    $(".grid-container").empty();
+
+    for (var i = 0; i < size; i++) {
+      for (var j = 0; j < size; j++) {
+        $(".grid-container").append(html);
+      }
+    }
+
+    $(".grid-box").css("width", 960/size);
+    $(".grid-box").css("height", 960/size);
+
+    $(".grid-box").hover(function() {
+      $(this).addClass("fill");
+    });
+  }
 
 });
